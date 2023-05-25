d5bc645dc51ae1c7a8dbc17b9094d414e17f0b6b
ydrea <ydrea.wrld@gmail.com>
2023-05-25 22:07:46 +0200

    on

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import './App.css';
-import 'leaflet/dist/leaflet.css'
-import {MapContainer, Popup, TileLayer} from 'react-leaflet'
-import { Marker } from 'react-leaflet';
-import  MarkerClusterGroup from 'react-leaflet-cluster';
-import { Icon } from 'leaflet';
+import { Mapa } from './comps/Mapa';
 
-const markers =[
-  {
-    geocode: [45.5, 13.5],
-    popUp: 'medo?'
-  },
-  {
-    geocode: [45.502, 13.5],
-    popUp: 'zeko!'
-  }, {
-    geocode: [45.501, 13.504],
-    popUp: 'ribicaa!'
-  }
-]
-
-const lIcon = new Icon({
-  iconUrl: require("./ikona.png"),
-  iconSize: [28, 28]
-})
-
-function App () {
-
-return (<>
-Rleaf
-  <MapContainer center={[45.501,13.51]} zoom={15}>
-<TileLayer attribution='OpenStreetMap' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-<MarkerClusterGroup>
-{markers.map((i)=>(
-  <Marker position={i.geocode} icon={lIcon}>
-    <Popup>{i.popUp}</Popup>
-  </Marker>
-))
-}
-</MarkerClusterGroup>
-  </MapContainer>
-
-  </>
-)
+//
+function App() {
+  //
+  return (
+    <>
+      Rleaf
+      <Mapa />
+    </>
+  );
 }
 export default App;
---
+++ src/comps/Mapa.js
@@ -0,0 +1,58 @@
+import '../App.css';
+import 'leaflet/dist/leaflet.css';
+import {
+  Marker,
+  GeoJSON,
+  MapContainer,
+  Popup,
+  TileLayer,
+} from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+//
+
+const markers = [
+  {
+    geocode: [45.5, 13.5],
+    popUp: 'medo?',
+  },
+  {
+    geocode: [45.502, 13.5],
+    popUp: 'zeko!',
+  },
+  {
+    geocode: [45.501, 13.504],
+    popUp: 'ribicaa!',
+  },
+];
+
+//
+export const Mapa = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Make the WFS request
+    axios
+      .get(
+        'https://starigrad.agr.unizg.hr/geoserver/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAMES=qgis2webtest:kopnena_stanista_2016&outputFormat=application/json&srsName=epsg:4326'
+      )
+      .then(response => {
+        setData(response.data); // Assuming the response contains the GeoJSON data
+      })
+      .catch(error => {
+        console.error('Error fetching WFS data:', error);
+      });
+  }, []);
+
+  return (
+    <MapContainer
+      center={[45.501, 13.5]}
+      zoom={13}
+      style={{ height: '100vh' }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {data && <GeoJSON data={data} />}
+    </MapContainer>
+  );
+};
