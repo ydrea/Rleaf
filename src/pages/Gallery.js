41cb13b29a4ca10fa536a8cd1de6f32d6a1f96f8
ydrea <ydrea.wrld@gmail.com>
2023-08-01 09:02:39 +0200

    slider-hider

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,7 @@
 import { Login } from './Login';
 import y from './gallery.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   getPhotos,
   increment,
@@ -15,34 +15,44 @@ export function Gallery() {
   //
   const dispatch = useDispatch();
   const fotos = useSelector(selectPhotos);
-  const idX = fotos.map(i => {
-    return i.id;
-  });
-  console.log(idX);
-  //minus
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    setSlide(slide === fotos.length - 1 ? 0 : slide + 1);
+  };
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? fotos.length - 1 : slide - 1);
+  }; //minus
   const ajNazad = () => {
-    dispatch(decrement(idX));
+    dispatch(decrement());
   };
   //plus
   const ajNapred = () => {
-    dispatch(increment(idX));
+    dispatch(increment());
   };
   //
   const images = fotos.map((i, index) => (
     <div className={y.slider}>
-      <p style={{ fontSize: 40 }} onClick={ajNazad}>
+      <p
+        style={{ fontSize: 40, cursor: 'crosshair' }}
+        onClick={prevSlide}
+      >
         &#10092;
       </p>
       <img
         className={y.slide}
-        key={i.id}
+        key={i.index}
         src={process.env.REACT_APP_SERVER_PUB + `/${i.signatura}`}
         width="1000"
       />
       <div className={y.exif}>
+        <div style={{ fontSize: 50, cursor: 'crosshair' }}>
+          {index}
+        </div>
         {i.naziv}, {i.opis}, {i.lokacija}
       </div>
-      <p style={{ fontSize: 40 }} onClick={ajNapred}>
+      <p style={{ fontSize: 40 }} onClick={nextSlide}>
         &#10093;
       </p>
     </div>
@@ -57,8 +67,38 @@ export function Gallery() {
 
   return (
     <div className={y.g}>
-      {images[3]}
-      <GalleryCard idX={idX} />
+      {fotos.map((i, idx) => {
+        return (
+          <div className={slide === idx ? y.slider : y.hid}>
+            <p
+              style={{ fontSize: 40, cursor: 'crosshair' }}
+              onClick={prevSlide}
+            >
+              &#10092;
+            </p>
+            <img
+              className={y.slide}
+              key={idx}
+              src={
+                process.env.REACT_APP_SERVER_PUB + `/${i.signatura}`
+              }
+            />
+            <div className={y.exif}>
+              <div style={{ fontSize: 50, cursor: 'crosshair' }}>
+                {idx}
+              </div>
+              {i.naziv}, {i.opis}, {i.lokacija}
+            </div>
+            <p
+              style={{ fontSize: 40, cursor: 'crosshair' }}
+              onClick={nextSlide}
+            >
+              &#10093;
+            </p>
+          </div>
+        );
+      })}
+      <GalleryCard />
       <Login />
     </div>
   );
