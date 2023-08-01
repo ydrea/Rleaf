459d69c0e8fac76551eba865ab5e0017a9befe68
ydrea <ydrea.wrld@gmail.com>
2023-08-01 11:20:00 +0200

    dz

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -24,39 +24,6 @@ export function Gallery() {
   const prevSlide = () => {
     setSlide(slide === 0 ? fotos.length - 1 : slide - 1);
   }; //minus
-  const ajNazad = () => {
-    dispatch(decrement());
-  };
-  //plus
-  const ajNapred = () => {
-    dispatch(increment());
-  };
-  //
-  const images = fotos.map((i, index) => (
-    <div className={y.slider}>
-      <p
-        style={{ fontSize: 40, cursor: 'crosshair' }}
-        onClick={prevSlide}
-      >
-        &#10092;
-      </p>
-      <img
-        className={y.slide}
-        key={i.index}
-        src={process.env.REACT_APP_SERVER_PUB + `/${i.signatura}`}
-        width="1000"
-      />
-      <div className={y.exif}>
-        <div style={{ fontSize: 50, cursor: 'crosshair' }}>
-          {index}
-        </div>
-        {i.naziv}, {i.opis}, {i.lokacija}
-      </div>
-      <p style={{ fontSize: 40 }} onClick={nextSlide}>
-        &#10093;
-      </p>
-    </div>
-  ));
 
   useEffect(() => {
     dispatch(getPhotos());
@@ -99,7 +66,7 @@ export function Gallery() {
           </div>
         );
       })}
-      <GalleryCard />
+      {/* <GalleryCard /> */}
       <Login />
     </div>
   );
