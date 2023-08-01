6dce0e48ae616be478ccb081b3905007c81a84ce
ydrea <ydrea.wrld@gmail.com>
2023-08-01 20:45:55 +0200

    Home = WFS

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,12 +10,21 @@ import {
   // WMSTileLayer,
 } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import iconUrl from '../assets/ikona.png'; // Check the path to the icon image
 
 import 'proj4'; // import required for side effect
 import 'proj4leaflet'; // import required for side effect
+
+import nationalParks from './national-parks.json';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 // const myIcon = new L.Icon({
 //   iconUrl: iconUrl.default,
@@ -48,62 +57,95 @@ import 'proj4leaflet'; // import required for side effect
 //
 
 export const About = () => {
-  // const [data, setData] = useState([]);
+//   // const [data, setData] = useState([]);
 
-  // const { BaseLayer, Overlay } = LayersControl;
-  // //
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       'https://landscape.agr.hr/qgis/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&outputFormat=application/json&srsName=epsg:4326&TYPENAME=naselja_stanovnistvo'
-  //     )
-  //     .then(response => {
-  //       setData(response.data); // Assuming the response contains the GeoJSON data
-  //       console.log(data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching WFS data:', error);
-  //     });
-  // }, [data]);
+//   const mapRef = useRef();
 
-  // const onEachFeature = async (feature, layer) => {
-  //   await layer.bindPopup(feature.properties.name);
-  // };
-  // const layer = new L.WFS({
-  //   url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs?',
-  //   typeNS: 'avoindata',
-  //   typeName: 'Kaupunginosajako',
-  //   crs: EPSG3879(),
-  //   style: {
-  //     color: 'blue',
-  //     weight: 2,
-  //   },
-  // });
-  //
+//   useEffect(() => {
+//     const { current = {} } = mapRef;
+//     const { leafletElement: map } = current;
 
-  // const helsinkiCoordinates = [60.192059, 24.945831];
-  // const crs = EPSG3879();
-  // const url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wms?'; // you may use any geoserver you know
-  // const wmsOptions = { layers: 'avoindata:Opaskartta_1940' }; // comma-separated string of any WMS layer(s) on the geoserver
-  // return (
-  //   <Map center={helsinkiCoordinates} zoom={8} minZoom={5} scrollWheelZoom={false} crs={crs}>
-  //     <WMSTileLayer url={url} wmsOptions={wmsOptions}/>
-  //   </Map>);
+//     if ( !map ) return;
 
-  return (
-    <>O nama</>
-    // <MapContainer
-    //   center={[45.2, 16.2]}
-    //   zoom={8}
-    //   style={{ height: '80vh' }}
-    // >
-    //   <LayersControl>
-    //     <BaseLayer checked name="OSM1">
-    //       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    //     </BaseLayer>
-    //   </LayersControl>
-    //   {layer}
-    //   {data && <GeoJSON data={data} onEachFeature={onEachFeature} />}
-    // </MapContainer>
-  );
-};
+
+//     const parksGeoJson = new L.GeoJSON(nationalParks, {
+//       onEachFeature: (feature = {}, layer) => {
+//         const { properties = {} } = feature;
+//         const { Name } = properties;
+
+//         if ( !Name ) return;
+
+//         layer.bindPopup(`<p>${Name}</p>`);
+//       }
+//     });
+
+//     parksGeoJson.addTo(map);
+//   }, [])
+// return (
+  
+//   <div className="App">
+//   <MapContainer ref={mapRef} center={[39.50, -98.35]} zoom={4}>
+//     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+//   </MapContainer>
+// </div>
+// );
+}
+
+//////////////////////////////////////////////////////
+// const { BaseLayer, Overlay } = LayersControl;
+// //
+// useEffect(() => {
+//   axios
+//     .get(
+//       'https://landscape.agr.hr/qgis/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&outputFormat=application/json&srsName=epsg:4326&TYPENAME=naselja_stanovnistvo'
+//     )
+//     .then(response => {
+//       setData(response.data); // Assuming the response contains the GeoJSON data
+//       console.log(data);
+//     })
+//     .catch(error => {
+//       console.error('Error fetching WFS data:', error);
+//     });
+// }, [data]);
+
+// const onEachFeature = async (feature, layer) => {
+//   await layer.bindPopup(feature.properties.name);
+// };
+// const layer = new L.WFS({
+//   url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wfs?',
+//   typeNS: 'avoindata',
+//   typeName: 'Kaupunginosajako',
+//   crs: EPSG3879(),
+//   style: {
+//     color: 'blue',
+//     weight: 2,
+//   },
+// });
+//
+
+// const helsinkiCoordinates = [60.192059, 24.945831];
+// const crs = EPSG3879();
+// const url = 'https://kartta.hel.fi/ws/geoserver/avoindata/wms?'; // you may use any geoserver you know
+// const wmsOptions = { layers: 'avoindata:Opaskartta_1940' }; // comma-separated string of any WMS layer(s) on the geoserver
+// return (
+//   <Map center={helsinkiCoordinates} zoom={8} minZoom={5} scrollWheelZoom={false} crs={crs}>
+//     <WMSTileLayer url={url} wmsOptions={wmsOptions}/>
+//   </Map>);
+
+// return (
+//   <>O nama</>
+// <MapContainer
+//   center={[45.2, 16.2]}
+//   zoom={8}
+//   style={{ height: '80vh' }}
+// >
+//   <LayersControl>
+//     <BaseLayer checked name="OSM1">
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//     </BaseLayer>
+//   </LayersControl>
+//   {layer}
+//   {data && <GeoJSON data={data} onEachFeature={onEachFeature} />}
+// </MapContainer>
+//   );
+// };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import L from 'leaflet';
+import geojson from '../data/temakz.geojson.json';
+import tileLayer from '../utils/tileLayer';
 
-function Home() {
-  return <div>Home</div>;
+const center = [45.2, 16.2];
+
+function onEachFeature(feature, layer) {
+  layer.bindPopup(feature.properties.code_i_opi);
 }
 
-export default Home;
+const MapWrapper = () => {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (!map) return;
+
+    const legend = L.control({ position: 'bottomleft' });
+
+    legend.onAdd = () => {
+      const div = L.DomUtil.create('div', 'legend');
+      div.innerHTML = `click on polygon`;
+      return div;
+    };
+
+    legend.addTo(map);
+  }, [map]);
+
+  return (
+    <MapContainer
+      whenCreated={setMap}
+      center={center}
+      zoom={9}
+      scrollWheelZoom={false}
+    >
+      <TileLayer {...tileLayer} />
+
+      <GeoJSON data={geojson} onEachFeature={onEachFeature} />
+    </MapContainer>
+  );
+};
+
+export default MapWrapper;
