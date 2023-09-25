5b1ac23c010ea861ab772660cbfb3598cd736305
ydrea <ydrea.wrld@gmail.com>
2023-09-25 17:24:32 +0200

    1 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -73,23 +73,22 @@ function Home() {
           </div>
 
           <div className="txt3">
+            {/* <div> */}
+            Pozivamo vas da se pridružite našem putovanju prema
+            očuvanju i razumijevanju krajobraza.
             <div>
-              Pozivamo vas da se pridružite našem putovanju prema
-              očuvanju i razumijevanju krajobraza.
-              <div>
-                Za sve informacije vezane uz ‘Opservatorij
-                krajobraza’, slobodno nas kontaktirajte putem e-maila
-                na [info@croatianlandscape.hr] ili telefonom na [+385
-                95 9123055].
-              </div>
-              <div>
-                Zanima vas više o našem radu i misiji? Pretplatite se
-                na naš newsletter i budite u tijeku s našim
-                istraživanjima i aktivnostima. Unesite svoju e-mail
-                adresu i pridružite se putovanju prema razumijevanju i
-                očuvanju krajobraza.
-              </div>
+              Za sve informacije vezane uz ‘Opservatorij krajobraza’,
+              slobodno nas kontaktirajte putem e-maila na
+              [info@croatianlandscape.hr] ili telefonom na [+385 95
+              9123055].
             </div>
+            <div>
+              Zanima vas više o našem radu i misiji? Pretplatite se na
+              naš newsletter i budite u tijeku s našim istraživanjima
+              i aktivnostima. Unesite svoju e-mail adresu i pridružite
+              se putovanju prema razumijevanju i očuvanju krajobraza.
+            </div>
+            {/* </div> */}
           </div>
         </div>
       </div>
