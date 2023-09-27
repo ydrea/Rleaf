6dd69671bd97adaafd7cdc5468337e77ea6742f3
ydrea <ydrea.wrld@gmail.com>
2023-09-27 23:18:56 +0200

    joscrte

--- src/pages/projects/ewap.js
+++ src/pages/projects/ewap.js
@@ -7,24 +7,32 @@ const Ewap = () => {
       <div className="projects-container">
         <img src="/proj2.jpg" />
         <h1 style={{ color: 'white' }}>
-          Ugrožena drvena arhitektura Banovine/Banije
+          Ugrožena drvena arhitektura
+          <br /> Banovine/Banije
         </h1>
       </div>
       <div className="tekst">
-        Projekt EWAP: Ugrožena drvena arhitektura Banovina/Banije,
-        Posavine i Pokuplja 2023-2024 dio je globalnog programa
-        dokumentacije ugrožene drvene arhitekture kojeg provodi Oxford
-        Brookes University iz Velike Britanije. Tim Opservatorija
-        krajobraza, putem formalnog nositelja projekta: Društva Terra
-        Banalis sa sjedištem u Glini, predložio je područje Banije,
-        Posavine i Pokuplja na kojemu se nalazi najveća koncentracija
-        povijesne drvene arhitekture u Hrvatskoj, koja je iz raznih
-        razloga danas ugrožena. Kroz osvojenu programsku podršku će se
-        tijekom 2023. i 2024. godine digitalno dokumentirati značajan
-        broj naseobina i pojedinačnih objekata, vještina, znanja i
-        umijeća vezanih uz obradu drveta i gradnju građevina od drvata
-        te obiteljskih i osobnih priča vlasnika i graditelja drvenih
-        kuća. Više o projektu možete pročitati na{' '}
+        <p>
+          {' '}
+          Projekt EWAP: Ugrožena drvena arhitektura Banovina/Banije,
+          Posavine i Pokuplja 2023-2024 dio je globalnog programa
+          dokumentacije ugrožene drvene arhitekture kojeg provodi
+          Oxford Brookes University iz Velike Britanije.
+        </p>
+        <p>
+          {' '}
+          Tim Opservatorija krajobraza, putem formalnog nositelja
+          projekta: Društva Terra Banalis sa sjedištem u Glini,
+          predložio je područje Banije, Posavine i Pokuplja na kojemu
+          se nalazi najveća koncentracija povijesne drvene arhitekture
+          u Hrvatskoj, koja je iz raznih razloga danas ugrožena.
+        </p>
+        Kroz osvojenu programsku podršku će se tijekom 2023. i 2024.
+        godine digitalno dokumentirati značajan broj naseobina i
+        pojedinačnih objekata, vještina, znanja i umijeća vezanih uz
+        obradu drveta i gradnju građevina od drvata te obiteljskih i
+        osobnih priča vlasnika i graditelja drvenih kuća. Više o
+        projektu možete pročitati na{' '}
         <a
           href="https://terrabanalis.wixsite.com/terrabanalis/drvena-ba%C5%A1tina-sm%C5%BE"
           target="_blank"
