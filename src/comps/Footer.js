48d8095989f6d5da56d63c05a95667449545be8b
ydrea <ydrea.wrld@gmail.com>
2023-09-27 11:22:51 +0200

    slidinng foot

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './footer.css';
-import logo from '../assets/Asset 5.svg';
+import logo from '../assets/Asset 0.svg';
 //
 export default function Footer() {
   return (
     <div className="footer">
-      {/* da)
-      <a className={s.a} href="https://github.com/ydrea">
-        ydrea
-      </a>
-      (mnation */}
-      <img src={logo} />
-      <p>
+      <div className="logo-container">
         {' '}
-        Uvjeti korištenja "Sve fotografije i tekstovi objavljeni na
-        ovom portalu podliježu CC BY-NC-SA 4.0 licenci. Ova licenca
-        dopušta nekomercijalno umnožavanje, dijeljenje i mijenjanje,
-        uz navođenje autorstva na način koji je specificirao autor i
-        uz daljnje dijeljenje pod istim uvjetima. Obratite nam se
-        ukoliko su vam potrebne fotografije u izvornoj veličini."
-      </p>
+        <img src={logo} alt="Logo" className="logo" />
+        <p className="roll">
+          {' '}
+          Uvjeti korištenja "Sve fotografije i tekstovi objavljeni na
+          ovom portalu podliježu CC BY-NC-SA 4.0 licenci. Ova licenca
+          dopušta nekomercijalno umnožavanje, dijeljenje i mijenjanje,
+          uz navođenje autorstva na način koji je specificirao autor i
+          uz daljnje dijeljenje pod istim uvjetima. Obratite nam se
+          ukoliko su vam potrebne fotografije u izvornoj veličini."
+        </p>
+      </div>
     </div>
   );
 }
