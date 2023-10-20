f2e6f00dc282ad5f842b470c30771905073e492a
ydrea <ydrea.wrld@gmail.com>
2023-10-20 15:57:26 +0200

    upload fix

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -9,83 +9,100 @@ import { getPhotos } from '../redux/rtk/gallerySlice';
 
 import './Fileupload.css';
 
-const decodeString = (value) => {
+const decodeString = value => {
   try {
     return decodeURIComponent(escape(value));
-  } catch(e) {
+  } catch (e) {
     //
   }
   return value;
-}
+};
 
-const getExifData = async (file) => {
+const getExifData = async file => {
   try {
     const data = await exifr.parse(file, { iptc: true, xmp: true });
-    return data
+    return data;
   } catch (err) {
     console.error('Error parsing EXIF data:', err);
-    return undefined
+    return undefined;
   }
 };
 
-const parseExifDate = (value) => {
+const parseExifDate = value => {
   if (!value) {
-    return undefined
+    return undefined;
   }
   if (typeof value === 'string' && value.length === 8) {
-    return `${value.slice(0, 4)}-${value.slice(4, 6)}-${value.slice(6, 8)}`
+    return `${value.slice(0, 4)}-${value.slice(4, 6)}-${value.slice(
+      6,
+      8
+    )}`;
   }
   try {
-    var d = new Date(value)
-    return new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString().slice(0, 10)
+    var d = new Date(value);
+    return new Date(d.getTime() - d.getTimezoneOffset() * 60000)
+      .toISOString()
+      .slice(0, 10);
   } catch (e) {
-    return undefined
+    return undefined;
   }
-}
+};
 
 const saveExif = async (signatura, exif = {}) => {
   try {
-    const res = await fetch(
-      `${process.env.REACT_APP_SERVER}/novi`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          signatura,
-          lokacija: exif.Location ?? '',
-          datum_sni: parseExifDate(exif.DateCreated),
-          autor: exif.Artist ?? '',
-          copyright: decodeString(exif.Copyright ?? exif.rights?.value ?? exif.CopyrightNotice ?? ''),
-          tagovi: (Array.isArray(exif.subject) ? exif.subject : []).join(','),
-          lon: `${exif.longitude ?? ''}`,
-          lat: `${exif.latitude ?? ''}`,
-          naziv: decodeString(exif.Title ?? exif.title?.value ?? exif.ObjectName ?? ''),
-          naziv_eng: '',
-          opis: decodeString(exif.ImageDescription ?? exif.description?.value ?? exif.Caption ?? ''),
-          opis_eng: '',
-          kategorija: '',
-          copyright_holder: '',
-          doi: '',
-        }),
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
+    const res = await fetch(`${process.env.REACT_APP_SERVER}/novi`, {
+      method: 'POST',
+      body: JSON.stringify({
+        signatura,
+        lokacija: exif.Location ?? '',
+        datum_sni: parseExifDate(exif.DateCreated),
+        autor: exif.Artist ?? '',
+        copyright: decodeString(
+          exif.Copyright ??
+            exif.rights?.value ??
+            exif.CopyrightNotice ??
+            ''
+        ),
+        tagovi: (Array.isArray(exif.subject)
+          ? exif.subject
+          : []
+        ).join(','),
+        lon: `${exif.longitude ?? ''}`,
+        lat: `${exif.latitude ?? ''}`,
+        naziv: decodeString(
+          exif.Title ?? exif.title?.value ?? exif.ObjectName ?? ''
+        ),
+        naziv_eng: '',
+        opis: decodeString(
+          exif.ImageDescription ??
+            exif.description?.value ??
+            exif.Caption ??
+            ''
+        ),
+        opis_eng: '',
+        kategorija: '',
+        copyright_holder: '',
+        doi: '',
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
     if (res.ok) {
-      const data = await res.json()
+      const data = await res.json();
       return {
         data,
-      }
+      };
     }
     return {
-      error: 'Error!'
-    }
+      error: 'Error!',
+    };
   } catch (err) {
     return {
       error: err?.msg ?? 'Error!',
-    }
+    };
   }
-}
+};
 
-const uploadFile = async (file) => {
+const uploadFile = async file => {
   const formData = new FormData();
   formData.append('file', file);
   try {
@@ -111,10 +128,10 @@ const uploadFile = async (file) => {
       };
     }
     return {
-      error: 'An error occurred while uploading the file'
+      error: 'An error occurred while uploading the file',
     };
   }
-}
+};
 
 export const Fileupload = () => {
   const dispatch = useDispatch();
@@ -125,23 +142,23 @@ export const Fileupload = () => {
   const [exifList, setExifList] = useState([]);
   const [savedExifList, setSavedExifList] = useState([]);
 
-  const [isSaving, setIsSaving] = useState(false)
-  const submitIsDisabled = fileList.length === 0 || isSaving
+  const [isSaving, setIsSaving] = useState(false);
+  const submitIsDisabled = fileList.length === 0 || isSaving;
 
   const onChange = e => {
-    const files = [...e.target.files]
-    setFileList(files)
+    const files = [...e.target.files];
+    setFileList(files);
   };
 
   const onSubmit = async e => {
     e.preventDefault();
 
     if (fileList.length === 0) {
-      return
+      return;
     }
 
-    setIsSaving(true)
-    setMessage('Saving...')
+    setIsSaving(true);
+    setMessage('Saving...');
 
     // if (fileList.length === 1) {
     //   const { data, error } = await uploadFile(fileList[0])
@@ -158,42 +175,53 @@ export const Fileupload = () => {
       files: fileList.length,
       uploads: 0,
       saves: 0,
-    }
-    await Promise.allSettled(fileList.map((file, fileIndex) => {
-      return uploadFile(file).then((uploadFileData) => {
-        if (uploadFileData.data) {
-          count.uploads += 1
-        }
-        setUploadedFileList((currentUploadedFileList) => currentUploadedFileList.map((item, index) => {
-          if (index !== fileIndex) {
-            return item
+    };
+    await Promise.allSettled(
+      fileList.map((file, fileIndex) => {
+        return uploadFile(file).then(uploadFileData => {
+          if (uploadFileData.data) {
+            count.uploads += 1;
           }
-          return uploadFileData
-        }))
-        if (uploadFileData.data) {
-          return saveExif(uploadFileData.data.fileName, exifList[fileIndex]).then((saveExifData) => {
-            if (saveExifData.data) {
-              count.saves += 1
-            }
-            setSavedExifList((currentSavedExifList) => currentSavedExifList.map((item, index) => {
+          setUploadedFileList(currentUploadedFileList =>
+            currentUploadedFileList.map((item, index) => {
               if (index !== fileIndex) {
-                return item
+                return item;
               }
-              return saveExifData
-            }))
-          })
-        }
+              return uploadFileData;
+            })
+          );
+          if (uploadFileData.data) {
+            return saveExif(
+              uploadFileData.data.fileName,
+              exifList[fileIndex]
+            ).then(saveExifData => {
+              if (saveExifData.data) {
+                count.saves += 1;
+              }
+              setSavedExifList(currentSavedExifList =>
+                currentSavedExifList.map((item, index) => {
+                  if (index !== fileIndex) {
+                    return item;
+                  }
+                  return saveExifData;
+                })
+              );
+            });
+          }
+        });
       })
-    }))
+    );
 
     setMessage(`${count.saves}/${count.files} images saved.`);
 
     dispatch(getPhotos());
   };
 
-  const handleRemove = async (photo) => {
+  const handleRemove = async photo => {
     setMessage('');
-    const photoLabel = `id: ${photo.id}${photo.signatura ? ` (${photo.signatura})` : ''}`
+    const photoLabel = `id: ${photo.id}${
+      photo.signatura ? ` (${photo.signatura})` : ''
+    }`;
     if (photo) {
       try {
         const res = await fetch(
@@ -205,46 +233,50 @@ export const Fileupload = () => {
         if (res.ok) {
           setMessage(`Record ${photoLabel} successfully deleted!`);
           dispatch(getPhotos());
-          return true
+          return true;
         }
       } catch (err) {
         console.error(err.msg, 'nece');
       }
     }
-    return false
-  }
+    return false;
+  };
 
   useEffect(() => {
-    let isMounted = true
-    const emptyList = fileList.map(() => undefined)
-    setExifList(emptyList)
-    setUploadedFileList(emptyList)
-    setSavedExifList(emptyList)
-    setIsSaving(false)
-    setMessage('')
+    let isMounted = true;
+    const emptyList = fileList.map(() => undefined);
+    setExifList(emptyList);
+    setUploadedFileList(emptyList);
+    setSavedExifList(emptyList);
+    setIsSaving(false);
+    setMessage('');
 
     const getExif = async () => {
-      const exifData = await Promise.allSettled(fileList.map((file) => getExifData(file)))
+      const exifData = await Promise.allSettled(
+        fileList.map(file => getExifData(file))
+      );
       if (isMounted) {
-        setExifList(exifData.map((result) => {
-          if (result.status === 'fulfilled') {
-            return result.value ?? undefined
-          }
-          return undefined
-        }))
+        setExifList(
+          exifData.map(result => {
+            if (result.status === 'fulfilled') {
+              return result.value ?? undefined;
+            }
+            return undefined;
+          })
+        );
       }
-    }
-    getExif()
+    };
+    getExif();
 
     return () => {
-      isMounted = false
-    }
+      isMounted = false;
+    };
   }, [fileList]);
 
   return (
     <>
       {message ? <Message msg={message} /> : null}
-      <form onSubmit={onSubmit}>
+      <form style={{ paddingTop: '100px' }} onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
             type="file"
@@ -263,9 +295,19 @@ export const Fileupload = () => {
               <li key={file.name}>
                 {isSaving ? (
                   <span>
-                    Uploaded {uploadedFileList[fileIndex] ? uploadedFileList[fileIndex].data ? '🟢' : '🔴' : '⚪️'}
+                    Uploaded{' '}
+                    {uploadedFileList[fileIndex]
+                      ? uploadedFileList[fileIndex].data
+                        ? '🟢'
+                        : '🔴'
+                      : '⚪️'}
                     {' | '}
-                    Saved {savedExifList[fileIndex] ? savedExifList[fileIndex].data ? '🟢' : '🔴' : '⚪️'}
+                    Saved{' '}
+                    {savedExifList[fileIndex]
+                      ? savedExifList[fileIndex].data
+                        ? '🟢'
+                        : '🔴'
+                      : '⚪️'}
                     {' | '}
                   </span>
                 ) : null}
@@ -282,7 +324,7 @@ export const Fileupload = () => {
           className="btn btn-primary btn-block mt-4"
         />
       </form>
-
+      <h1>123</h1>
       {/* {uploadedFileList.length === 1 && !!uploadedFileList[0] ? (
         <>
           <div className="row mt-5">
@@ -299,7 +341,6 @@ export const Fileupload = () => {
           <FormA uploadedFile={uploadedFileList[0]} exifR={exifList[0]} />
         </>
       ) : null} */}
-
       <PhotosEDIT onRemove={handleRemove} />
     </>
   );
