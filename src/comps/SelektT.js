d5ada91c9564136f7e31013d3432d8a132c6d6ce
ydrea <ydrea.wrld@gmail.com>
2023-12-21 12:56:27 +0100

    improved

--- src/comps/SelektT.js
+++ src/comps/SelektT.js
@@ -3,42 +3,38 @@ import Select from 'react-select';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilters } from '../redux/rtk/gallerySlice';
 
-const TagoviSelekt = ({ tagoviOptions }) => {
+function TagoviSelekt({ tagoviOptions }) {
 	const dispatch = useDispatch();
-	const [filterSelected, setFilterSelected] = useState([]);
-
 	const selectedFilters = useSelector(
 		state => state.gallery.selectedFilters
 	);
+	const [filterSelected, setFilterSelected] = useState([]);
+
+	useEffect(() => {
+		// Initialize the state with selected filters when the component mounts
+		const initialSelectedFilters = selectedFilters?.tagovi || [];
+		setFilterSelected(
+			initialSelectedFilters.map(option => ({
+				label: option,
+				value: option,
+			}))
+		);
+	}, [selectedFilters]);
+
 	const handleTagoviFilterChange = selectedFilters => {
 		const selectedFilterValues = selectedFilters.map(
 			filter => filter.value
 		);
 		dispatch(setFilters({ tagovi: selectedFilterValues }));
+		// Update the local state when filters change
+		setFilterSelected(
+			selectedFilters.map(option => ({
+				label: option.label,
+				value: option.value,
+			}))
+		);
 	};
-	useEffect(() => {
-		// Update the local state when selectedFilters change in Redux
-		setFilterSelected(prevState => ({
-			...prevState,
-			tagovi:
-				selectedFilters?.tagovi.map(value => ({
-					value,
-					label: value,
-				})) || [],
-		}));
-	}, [selectedFilters]);
 
-	useEffect(() => {
-		console.log(tagoviOptions, selectedFilters);
-	}, [tagoviOptions]);
-	//
-	// const handleTagoviFilterChange = selectedFilters => {
-	// 	const selectedFilterValues = selectedFilters.map(
-	// 		filter => filter.value
-	// 	);
-	// 	dispatch(setFilters({ tagovi: selectedFilterValues }));
-	// };
-	//
 	const cusTom = {
 		control: styles => ({
 			...styles,
@@ -75,7 +71,7 @@ const TagoviSelekt = ({ tagoviOptions }) => {
 				isSearchable
 				isMulti
 				options={tagoviOptions}
-				value={filterSelected.tagovi}
+				value={filterSelected}
 				onChange={handleTagoviFilterChange}
 			/>
 			<label
@@ -86,6 +82,97 @@ const TagoviSelekt = ({ tagoviOptions }) => {
 			</label>
 		</div>
 	);
-};
+}
 
 export default TagoviSelekt;
+// import React, { useState, useEffect } from 'react';
+// import Select from 'react-select';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { setFilters } from '../redux/rtk/gallerySlice';
+
+// const TagoviSelekt = ({ tagoviOptions }) => {
+// 	const dispatch = useDispatch();
+// 	const [filterSelected, setFilterSelected] = useState([]);
+
+// 	const selectedFilters = useSelector(
+// 		state => state.gallery.selectedFilters
+// 	);
+// 	const handleTagoviFilterChange = selectedFilters => {
+// 		const selectedFilterValues = selectedFilters.map(
+// 			filter => filter.value
+// 		);
+// 		dispatch(setFilters({ tagovi: selectedFilterValues }));
+// 	};
+// 	useEffect(() => {
+// 		// Update the local state when selectedFilters change in Redux
+// 		setFilterSelected(prevState => ({
+// 			...prevState,
+// 			tagovi:
+// 				selectedFilters?.tagovi.map(value => ({
+// 					value,
+// 					label: value,
+// 				})) || [],
+// 		}));
+// 	}, [selectedFilters]);
+
+// 	useEffect(() => {
+// 		console.log(tagoviOptions, selectedFilters);
+// 	}, [tagoviOptions]);
+// 	//
+// 	// const handleTagoviFilterChange = selectedFilters => {
+// 	// 	const selectedFilterValues = selectedFilters.map(
+// 	// 		filter => filter.value
+// 	// 	);
+// 	// 	dispatch(setFilters({ tagovi: selectedFilterValues }));
+// 	// };
+// 	//
+// 	const cusTom = {
+// 		control: styles => ({
+// 			...styles,
+// 			backgroundColor: 'transparent',
+// 			border: 'none',
+// 			borderTop: '1px solid black',
+// 			borderRadius: '0',
+// 			cursor: 'pointer',
+// 		}),
+// 		option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+// 			console.log('selekt', data, isDisabled, isFocused, isSelected);
+// 			return { ...styles, color: 'black', cursor: 'pointer' };
+// 		},
+// 		placeholder: styles => ({
+// 			...styles,
+// 			color: '#18aa00',
+// 			marginTop: '-13px',
+// 			marginLeft: '-8px',
+// 			fontSize: '22px',
+// 		}),
+// 		dropdownIndicator: provided => ({ ...provided, display: 'none' }),
+// 		indicatorSeparator: provided => ({
+// 			...provided,
+// 			display: 'none',
+// 		}),
+// 		menu: styles => ({ ...styles, zIndex: '999', cursor: 'pointer' }),
+// 	};
+
+// 	return (
+// 		<div>
+// 			<Select
+// 				styles={cusTom}
+// 				placeholder='unesi/odaberi'
+// 				isSearchable
+// 				isMulti
+// 				options={tagoviOptions}
+// 				value={filterSelected.tagovi}
+// 				onChange={handleTagoviFilterChange}
+// 			/>
+// 			<label
+// 				htmlFor='keyword-select'
+// 				style={{ color: '#7e7e77', fontSize: '22px' }}
+// 			>
+// 				ključne riječi
+// 			</label>
+// 		</div>
+// 	);
+// }
+
+// export default TagoviSelekt;
